a5839863b32ed655e6acfa92c4a26cb4846fbc9a
Donato <cruzmdonato@gmail.com>
2015-12-12 12:09:10 -0500

    initialized repo.

---
+++ chapter_02/exercises.js
@@ -0,0 +1,9 @@
+/*
+    Looping a triangle
+
+    Write a loop that makes seven calls to console.log to output the following triangle:
+*/
+
+for (var i = 2; i <= 8; i++) {
+    console.log(Array(i).join("#"));
+}
